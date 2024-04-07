9ae41206383f613915bd6ecbf91392fadabe8693
Gaurav <gaurav.behera@research.iiit.ac.in>
2024-04-08 00:33:45 +0530

    Added basic omp

--- src/main.cpp
+++ src/main.cpp
@@ -7,6 +7,7 @@
 #include <filesystem>
 #include <immintrin.h>
 #include <string>
+#include <omp.h>
 
 namespace solution
 {
@@ -33,37 +34,43 @@ namespace solution
 		bitmap_fs.read(reinterpret_cast<char *>(img), sizeof(float) * num_rows * num_cols);
 		bitmap_fs.close();
 
-		std::int32_t k = 0;
-		for (k = 0; k < num_rows * num_cols; k += 16)
+#pragma omp parallel for collapse(2) shared(img, result, sol_fs)
+		for (int i = 0; i < num_rows; ++i)
 		{
-			int i = k / num_cols, j = k % num_cols;
-			__m512 sum = _mm512_setzero_ps();
-			for (int di = -1; di <= 1; di++)
+			for (int j = 0; j < num_cols; j += 16)
 			{
-				for (int dj = -1; dj <= 1; dj++)
+				__m512 sum = _mm512_setzero_ps();
+				for (int di = -1; di <= 1; di++)
 				{
-					int ni = i + di, nj = j + dj;
-
-					if (ni >= 0 && ni < num_rows)
+					for (int dj = -1; dj <= 1; dj++)
 					{
-						__mmask16 mask = 0xFFFF;
-						if (nj < 0)
-						{
-							mask &= 0xFFFE;
-						}
-						if (nj + 15 >= num_cols)
-						{
-							mask &= 0x7FFF;
-						}
-						__m512 pixels = _mm512_mask_loadu_ps(_mm512_setzero_ps(), mask, &img[ni * num_cols + nj]);
+						int ni = i + di, nj = j + dj;
 
-						__m512 filterVal = _mm512_set1_ps(kernel[di + 1][dj + 1]);
-						sum = _mm512_fmadd_ps(pixels, filterVal, sum);
+						if (ni >= 0 && ni < num_rows)
+						{
+							__mmask16 mask = 0xFFFF;
+							if (nj < 0)
+							{
+								mask &= 0xFFFE;
+							}
+							if (nj + 15 >= num_cols)
+							{
+								mask &= 0x7FFF;
+							}
+							__m512 pixels = _mm512_mask_loadu_ps(_mm512_setzero_ps(), mask, &img[ni * num_cols + nj]);
+
+							__m512 filterVal = _mm512_set1_ps(kernel[di + 1][dj + 1]);
+							sum = _mm512_fmadd_ps(pixels, filterVal, sum);
+						}
 					}
 				}
+				_mm512_store_ps(result, sum);
+				#pragma omp critical
+                {
+                    sol_fs.write(reinterpret_cast<const char *>(result), sizeof(float) * 16);
+                }
+				// sol_fs.write(reinterpret_cast<const char *>(result), sizeof(float) * 16);
 			}
-			_mm512_store_ps(result, sum);
-			sol_fs.write(reinterpret_cast<const char *>(result), sizeof(float) * 16);
 		}
 		sol_fs.close();
 		free(result);
