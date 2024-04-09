4d187ef6db9ef4275c650d1a4c5a0596e38dee11
Gaurav <gaurav.behera@research.iiit.ac.in>
2024-04-09 10:41:52 +0530

    corrected error

--- src/main.cpp
+++ src/main.cpp
@@ -37,37 +37,44 @@ namespace solution
 		bitmap_fs.read(reinterpret_cast<char *>(img), sizeof(float) * num_rows * num_cols);
 		bitmap_fs.close();
 
-		set_num_threads(24);
-		#pragma omp parallel for collapse(2)
-		for (int i = 0; i < num_rows; ++i)
+// omp_set_num_threads(24);
+#pragma omp parallel
 		{
-			for (int j = 0; j < num_cols; j += 16)
+#pragma omp single
 			{
-				__m512 sum = _mm512_setzero_ps();
-				for (int di = -1; di <= 1; di++)
+				std::cout << omp_get_num_threads() << std::endl;
+			}
+#pragma omp for collapse(2)
+			for (int i = 0; i < num_rows; ++i)
+			{
+				for (int j = 0; j < num_cols; j += 16)
 				{
-					for (int dj = -1; dj <= 1; dj++)
+					__m512 sum = _mm512_setzero_ps();
+					for (int di = -1; di <= 1; di++)
 					{
-						int ni = i + di, nj = j + dj;
-
-						if (ni >= 0 && ni < num_rows)
+						for (int dj = -1; dj <= 1; dj++)
 						{
-							__mmask16 mask = 0xFFFF;
-							if (nj < 0)
-								mask &= 0xFFFE;
-							if (nj + 15 >= num_cols)
-								mask &= 0x7FFF;
+							int ni = i + di, nj = j + dj;
 
-							__m512 pixels = _mm512_mask_loadu_ps(_mm512_setzero_ps(), mask, &img[ni * num_cols + nj]);
-							__m512 filterVal = _mm512_set1_ps(kernel[di + 1][dj + 1]);
-							sum = _mm512_fmadd_ps(pixels, filterVal, sum);
+							if (ni >= 0 && ni < num_rows)
+							{
+								__mmask16 mask = 0xFFFF;
+								if (nj < 0)
+									mask &= 0xFFFE;
+								if (nj + 15 >= num_cols)
+									mask &= 0x7FFF;
+
+								__m512 pixels = _mm512_mask_loadu_ps(_mm512_setzero_ps(), mask, &img[ni * num_cols + nj]);
+								__m512 filterVal = _mm512_set1_ps(kernel[di + 1][dj + 1]);
+								sum = _mm512_fmadd_ps(pixels, filterVal, sum);
+							}
 						}
 					}
+					_mm512_storeu_ps(&result[i * num_cols + j], sum);
 				}
-				_mm512_storeu_ps(&result[i * num_cols + j], sum);
 			}
 		}
-		
+
 		sol_fs.write(reinterpret_cast<const char *>(result), sizeof(float) * num_rows * num_cols);
 		sol_fs.close();
 		free(result);
