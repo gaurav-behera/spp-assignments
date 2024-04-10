a36ccdfe34b3279b3685adcafa3f7f4ee69dc5e6
Gaurav <gaurav.behera@research.iiit.ac.in>
2024-04-10 12:30:47 +0530

    fixed mask

--- src/main.cpp
+++ src/main.cpp
@@ -47,12 +47,11 @@ namespace solution
 
 						if (ni >= 0 && ni < num_rows)
 						{
-
 							__m512 pixels = _mm512_loadu_ps(&img[ni * num_cols + nj]);
 							if (nj < 0)
-								pixels = _mm512_mask_blend_ps(32767, zero_vec, pixels);
-							if (nj + 15 >= num_cols)
-								pixels = _mm512_mask_blend_ps(65534, zero_vec, pixels);
+								pixels = _mm512_mask_blend_ps(32767, pixels,zero_vec);
+							else if (nj + 15 >= num_cols)
+								pixels = _mm512_mask_blend_ps(65534, pixels,zero_vec);
 
 							__m512 filterVal = _mm512_set1_ps(kernel[di + 1][dj + 1]);
 							sum = _mm512_fmadd_ps(pixels, filterVal, sum);
