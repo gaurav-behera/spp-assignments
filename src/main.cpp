f59419ef6204dae9382e22973ee4cb7aef7caca2
Gaurav <gaurav.behera@research.iiit.ac.in>
2024-05-08 10:13:31 +0530

    fixed variable values

--- src/main.cpp
+++ src/main.cpp
@@ -15,17 +15,19 @@ namespace solution
 	{
 		std::string sol_path = std::filesystem::temp_directory_path() / "student_sol.dat";
 
+		int size = 16777216;
+
 		int m1_fd = open(m1_path.c_str(), O_RDONLY);
-		float *m1 = static_cast<float *>(mmap(NULL, sizeof(float) * n * k, PROT_READ, MAP_PRIVATE, m1_fd, 0));
+		float *m1 = static_cast<float *>(mmap(NULL, size, PROT_READ, MAP_PRIVATE, m1_fd, 0));
 		int m2_fd = open(m2_path.c_str(), O_RDONLY);
-		float *m2 = static_cast<float *>(mmap(NULL, sizeof(float) * k * m, PROT_READ, MAP_PRIVATE, m2_fd, 0));
+		float *m2 = static_cast<float *>(mmap(NULL, size, PROT_READ, MAP_PRIVATE, m2_fd, 0));
 
 		int result_fd = open(sol_path.c_str(), O_CREAT | O_RDWR, 0644);
-		ftruncate(result_fd, sizeof(float) * n * m);
-		float *result = static_cast<float *>(mmap(NULL, sizeof(float) * n * m, PROT_WRITE | PROT_READ, MAP_SHARED, result_fd, 0));
+		ftruncate(result_fd, size);
+		float *result = static_cast<float *>(mmap(NULL, size, PROT_WRITE | PROT_READ, MAP_SHARED, result_fd, 0));
 
-		int block_size = 128;
-		int block_count = n / block_size;
+		// int block_size = 128;
+		// int block_count = n / block_size;
 
 #pragma omp parallel num_threads(48)
 		{
@@ -35,32 +37,30 @@ namespace solution
 			pthread_setaffinity_np(pthread_self(), sizeof(cpu_set_t), &cpuset);
 
 #pragma omp for collapse(2)
-			for (int block_i = 0; block_i < block_count; block_i++)
+			for (int block_i = 0; block_i < 16; block_i++)
 			{
-				for (int block_j = 0; block_j < block_count; block_j++)
+				for (int block_j = 0; block_j < 16; block_j++)
 				{
-					float temp[block_size * block_size] = {0};
-					for (int sub_block_k = 0; sub_block_k < block_count; sub_block_k++)
+					float temp[16384] = {0};
+					for (int sub_block_k = 0; sub_block_k < 16; sub_block_k++)
 					{
-						for (int idx = 0; idx < block_size; idx++)
+						for (int idx = 0; idx < 128; idx++)
 						{
-							for (int i = 0; i < block_size; i++)
+							for (int i = 0; i < 128; i++)
 							{
-								for (int j = 0; j < block_size; j += 16)
+								for (int j = 0; j < 128; j += 16)
 								{
-									int base1 = (i + block_i * block_size) * n + (sub_block_k * block_size + idx);
-									int base2 = (sub_block_k * block_size + idx) * n + (block_j * block_size + j);
-									int temp_base = i * block_size + j;
-									// int final_base = (i + block_i * block_size) * n + (j + block_j * block_size);
+									int base1 = (i + block_i * 128) * n + (sub_block_k * 128 + idx);
+									int base2 = (sub_block_k * 128 + idx) * n + (block_j * 128 + j);
+									int temp_base = i * 128 + j;
 									_mm512_storeu_ps(&temp[temp_base], _mm512_fmadd_ps(_mm512_set1_ps(m1[base1]), _mm512_loadu_ps(&m2[base2]), _mm512_loadu_ps(&temp[temp_base])));
-									// _mm512_storeu_ps(&result[final_base], _mm512_fmadd_ps(_mm512_set1_ps(m1[base1]), _mm512_loadu_ps(&m2[base2]), _mm512_loadu_ps(&result[final_base])));
 								}
 							}
 						}
 					}
-					for (int i = 0; i < block_size; i++)
+					for (int i = 0; i < 128; i++)
 					{
-						memcpy(&result[(block_i * block_size + i) * n + (block_j * block_size)], &temp[i * block_size], block_size*sizeof(float));
+						memcpy(&result[(block_i * 128 + i) * 2048 + (block_j * 128)], &temp[i * 128], 512);
 					}
 				}
 			}
