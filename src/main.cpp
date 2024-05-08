b8abf5fede8e0d4e20efd3421121977a939956ab
Gaurav <gaurav.behera@research.iiit.ac.in>
2024-05-08 18:47:06 +0530

    mem aligned with memadvise

--- src/main.cpp
+++ src/main.cpp
@@ -8,21 +8,24 @@
 #include <unistd.h>
 #include <fcntl.h>
 #include <sys/mman.h>
+#include <cstdlib>
 
 namespace solution
 {
 	std::string compute(const std::string &m1_path, const std::string &m2_path, int n, int k, int m)
 	{
-		std::string sol_path = std::filesystem::temp_directory_path() / "student_sol.dat";
+		std::string sol_path = "student_sol.dat";
 
 		int size = 16777216;
 
 		int m1_fd = open(m1_path.c_str(), O_RDONLY);
-		posix_fadvise(m1_fd, 0, 0, POSIX_FADV_SEQUENTIAL);
-		float *m1 = static_cast<float *>(mmap(NULL, size, PROT_READ, MAP_PRIVATE, m1_fd, 0));
+		void *m1_ptr;
+		posix_memalign(&m1_ptr, 64, size);
+		float *m1 = static_cast<float *>(mmap(m1_ptr, size, PROT_READ, MAP_PRIVATE, m1_fd, 0));
 		int m2_fd = open(m2_path.c_str(), O_RDONLY);
-		posix_fadvise(m2_fd, 0, 0, POSIX_FADV_SEQUENTIAL);
-		float *m2 = static_cast<float *>(mmap(NULL, size, PROT_READ, MAP_PRIVATE, m2_fd, 0));
+		void *m2_ptr;
+		posix_memalign(&m2_ptr, 64, size);
+		float *m2 = static_cast<float *>(mmap(m2_ptr, size, PROT_READ, MAP_PRIVATE, m2_fd, 0));
 
 		int result_fd = open(sol_path.c_str(), O_CREAT | O_RDWR, 0644);
 		ftruncate(result_fd, size);
@@ -56,7 +59,7 @@ namespace solution
 									int base1 = (i + block_i * 128) * n + (sub_block_k * 128 + idx);
 									int base2 = (sub_block_k * 128 + idx) * n + (block_j * 128 + j);
 									int temp_base = i * 128 + j;
-									_mm512_store_ps(&temp[temp_base], _mm512_fmadd_ps(_mm512_set1_ps(m1[base1]), _mm512_loadu_ps(&m2[base2]), _mm512_load_ps(&temp[temp_base])));
+									_mm512_store_ps(&temp[temp_base], _mm512_fmadd_ps(_mm512_set1_ps(m1[base1]), _mm512_load_ps(&m2[base2]), _mm512_load_ps(&temp[temp_base])));
 								}
 							}
 						}
