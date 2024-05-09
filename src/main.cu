7f82ccc74abeca7b7a6d15274ced776f55b51911
Gaurav <gaurav.behera@research.iiit.ac.in>
2024-05-09 20:56:57 +0530

    no shared

--- src/main.cu
+++ src/main.cu
@@ -25,42 +25,25 @@ namespace solution
         }
 
         #define TILE_WIDTH 32
-        __global__ void convolution2D(float *img_d, float *kernel_d, float* result_d, int n, int gpu_id, int gpu_count)
+        __global__ void convolution2D(float *img_d, float *kernel_d, float* result_d, int n)
         {
-                // __shared__ float img_s[TILE_WIDTH][TILE_WIDTH];
-                // __shared__ float kernel_s[3][3];
-
-                int tx = threadIdx.x, ty = threadIdx.y;
-                int col = blockIdx.x * blockDim.x + tx;
-                int row = blockIdx.y * blockDim.y + ty + gpu_id*n/gpu_count;
+                int col = blockIdx.x * blockDim.x + threadIdx.x;
+                int row = blockIdx.y * blockDim.y + threadIdx.y;
                 if (row < n && col < n)
                 {
-                        // if (tx < 3 && ty < 3)
-                        //         kernel_s[tx][ty] = kernel_d[tx*3+ty];
-                        // img_s[tx][ty] = img_d[row*n+col];
-                        __syncthreads();
                         float sum = 0.0;
                         for(int di = -1; di <= 1; di++)
                         {
                                 for(int dj = -1; dj <= 1; dj++) 
                                 {
-                                        // int ni = ty + di, nj = tx + dj;
-                                        if (row+di >= 0 && col+dj >= 0 && row+di < n && col+dj < n)
+                                        int ni = row + di, nj = col + dj;
+                                        if(ni >= 0 and ni < n and nj >= 0 and nj < n) 
                                         {
-                                                // if(ni >= 0 && ni < TILE_WIDTH && nj >= 0 && nj < TILE_WIDTH) 
-                                                // {
-                                                //         sum += kernel_s[di+1][dj+1] * img_s[ni][nj];
-                                                // }
-                                                // else
-                                                {
-                                                        sum += kernel_d[(di+1)*3+dj+1] * img_d[(row+di) * n + (col+dj)];
-                                                        // sum += kernel_s[di+1][dj+1] * img_d[(row+di) * n + (col+dj)];
-                                                }
-                                                
+                                                sum += kernel_d[(di+1)*3 + dj+1] * img_d[ni * n + nj];
                                         }
                                 }
                         }
-                        result_d[(row - gpu_id*n/gpu_count)*n+col] = sum;
+                        result_d[row*n+col] = sum;
                         // result_d[row*n+col] = img_d[row*n+col];
                 }
         }
@@ -75,7 +58,7 @@ namespace solution
                 float *img = static_cast<float *>(mmap(NULL, sizeof(float) * size, PROT_READ, MAP_PRIVATE, bitmap_fd, 0));
 
                 int result_fd = open(sol_path.c_str(), O_CREAT | O_RDWR, 0644);
-                if (ftruncate(result_fd, sizeof(float) * size) != 0) return sol_path;
+                ftruncate(result_fd, sizeof(float) * size);
                 float *result = reinterpret_cast<float *>(mmap(NULL, sizeof(float) * size, PROT_WRITE | PROT_READ, MAP_SHARED, result_fd, 0));
 
                 float kernel_flat[9];
@@ -83,30 +66,24 @@ namespace solution
                 {
                         kernel_flat[i] = kernel[i/3][i%3];
                 }
-                int gpu_count = 1;
-                #pragma omp parallel for num_threads(gpu_count)
-                for (int gpu_id = 0; gpu_id < gpu_count; gpu_id++)
-                {
-                        cudaSetDevice(gpu_id);
-                        float *img_d, *kernel_d, *result_d;
-                        CUDA_ERROR_CHECK(cudaMalloc((void**)&img_d, size * sizeof(float)));
-                        CUDA_ERROR_CHECK(cudaMemcpy(img_d, img, size * sizeof(float), cudaMemcpyHostToDevice));
-        
-                        CUDA_ERROR_CHECK(cudaMalloc((void**)&kernel_d, 9 * sizeof(float)));
-                        CUDA_ERROR_CHECK(cudaMemcpy(kernel_d, kernel_flat, 9 * sizeof(float), cudaMemcpyHostToDevice));
-        
-                        CUDA_ERROR_CHECK(cudaMalloc((void **)&result_d, (size/gpu_count) * sizeof(float)));
-        
-                        dim3 DimGrid(num_rows / (gpu_count * TILE_WIDTH), num_cols / TILE_WIDTH, 1);
-                        dim3 DimBlock(TILE_WIDTH, TILE_WIDTH, 1);
+                cudaSetDevice(0);
 
-                        convolution2D<<<DimGrid, DimBlock>>>(img_d, kernel_d, result_d, num_cols, gpu_id, gpu_count);
-                        
-                        cudaDeviceSynchronize();
-                        
-                        CUDA_ERROR_CHECK(cudaMemcpy(result + (size/gpu_count)*gpu_id, result_d, (size/gpu_count) * sizeof(float) , cudaMemcpyDeviceToHost));
-                }
+                float *img_d, *kernel_d, *result_d;
+                CUDA_ERROR_CHECK(cudaMalloc((void**)&img_d, size * sizeof(float)));
+                CUDA_ERROR_CHECK(cudaMemcpy(img_d, img, size * sizeof(float), cudaMemcpyHostToDevice));
 
+                CUDA_ERROR_CHECK(cudaMalloc((void**)&kernel_d, 9 * sizeof(float)));
+                CUDA_ERROR_CHECK(cudaMemcpy(kernel_d, kernel_flat, 9 * sizeof(float), cudaMemcpyHostToDevice));
+
+                CUDA_ERROR_CHECK(cudaMalloc((void **)&result_d, size * sizeof(float)));
+
+                dim3 DimGrid(num_rows / TILE_WIDTH, num_cols / TILE_WIDTH, 1);
+                dim3 DimBlock(TILE_WIDTH, TILE_WIDTH, 1);
+                convolution2D<<<DimGrid, DimBlock>>>(img_d, kernel_d, result_d, num_cols);
+                
+                cudaDeviceSynchronize();
+                
+                CUDA_ERROR_CHECK(cudaMemcpy(result, result_d, num_rows * num_cols * sizeof(float), cudaMemcpyDeviceToHost));
 
                 return sol_path;
         }
