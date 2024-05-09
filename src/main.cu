3d3255337d5a62cc3af711d1f183cca035b14e44
Gaurav <gaurav.behera@research.iiit.ac.in>
2024-05-09 13:50:25 +0530

    split across 4 gpus

--- src/main.cu
+++ src/main.cu
@@ -57,37 +57,38 @@ namespace solution
                 int size = num_rows * num_cols;
 
                 int bitmap_fd = open(bitmap_path.c_str(), O_RDONLY);
-		float *img = static_cast<float *>(mmap(NULL, sizeof(float) * size, PROT_READ, MAP_PRIVATE, bitmap_fd, 0));
+                float *img = static_cast<float *>(mmap(NULL, sizeof(float) * size, PROT_READ, MAP_PRIVATE, bitmap_fd, 0));
 
                 int result_fd = open(sol_path.c_str(), O_CREAT | O_RDWR, 0644);
-		ftruncate(result_fd, sizeof(float) * size);
-		float *result = reinterpret_cast<float *>(mmap(NULL, sizeof(float) * size, PROT_WRITE | PROT_READ, MAP_SHARED, result_fd, 0));
+                ftruncate(result_fd, sizeof(float) * size);
+                float *result = reinterpret_cast<float *>(mmap(NULL, sizeof(float) * size, PROT_WRITE | PROT_READ, MAP_SHARED, result_fd, 0));
 
                 float kernel_flat[9];
                 for (int i = 0; i < 9; i++)
                 {
-                        kernel_flat[i] = kernel[i/3][i%3];
+                        kernel_flat[i] = kernel[i / 3][i % 3];
                 }
-                
-                cudaSetDevice(0);
-                
-                float *img_d, *kernel_d, *result_d;
-                CUDA_ERROR_CHECK(cudaMalloc((void**)&img_d, size*sizeof(float)));
-                CUDA_ERROR_CHECK(cudaMemcpy(img_d, img, size*sizeof(float), cudaMemcpyHostToDevice));
 
-                CUDA_ERROR_CHECK(cudaMalloc((void**)&kernel_d, 9 * sizeof(float)));
+                cudaSetDevice(0);
+
+                float *img_d, *kernel_d, *result_d;
+                CUDA_ERROR_CHECK(cudaMalloc((void **)&img_d, size * sizeof(float)));
+                CUDA_ERROR_CHECK(cudaMemcpy(img_d, img, size * sizeof(float), cudaMemcpyHostToDevice));
+
+                CUDA_ERROR_CHECK(cudaMalloc((void **)&kernel_d, 9 * sizeof(float)));
                 CUDA_ERROR_CHECK(cudaMemcpy(kernel_d, kernel_flat, 9 * sizeof(float), cudaMemcpyHostToDevice));
 
-                CUDA_ERROR_CHECK(cudaMalloc((void **) &result_d, size*sizeof(float)));
+                CUDA_ERROR_CHECK(cudaMalloc((void **)&result_d, size * sizeof(float)));
 
-                dim3 DimGrid(num_rows/8, num_cols/8, 1);
-                dim3 DimBlock(8,8,1);
+                dim3 DimGrid(num_rows / 8, num_cols / 8, 1);
+                dim3 DimBlock(8, 8, 1);
                 convolution2D<<<DimGrid, DimBlock>>>(img_d, kernel_d, result_d, num_cols);
 
                 CUDA_ERROR_CHECK(cudaDeviceSynchronize());
 
-                cudaMemcpy(result, result_d, size*sizeof(float), cudaMemcpyDeviceToHost);
+                cudaMemcpy(result, result_d, size * sizeof(float), cudaMemcpyDeviceToHost);
 
                 return sol_path;
         }
+
 };
